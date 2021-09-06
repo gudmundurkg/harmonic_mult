a0d2644da39c716fb15ff61b7f59e4f6e9d08893
Gudmundur Kristjan <gummigudna@gmail.com>
2021-09-06 12:51:50 +0000

    Algorithm created

---
+++ harmonic_mult.py
@@ -0,0 +1,16 @@
+#Request input (h)armonic, (m)ultiplication or (q)uit
+    #if (q)
+        #Quit
+    #elif (h)
+        #print first n numbers in Harmonc Series
+        #One result per line
+        #Sum at bottom
+        #All numbers rounded with 4 extra digits
+    #elif (m)
+        #print ancient egyption multiplication
+        #print every step along the way
+    #else
+        #print ("Illegal Choice")
+
+   
+    
