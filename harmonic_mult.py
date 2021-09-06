b4894bbc694fd0f5b7c697cb4ce02eca7dbb52ab
Gudmundur Kristjan <gummigudna@gmail.com>
2021-09-06 15:32:40 +0000

    added code for choices m and q

--- harmonic_mult.py
+++ harmonic_mult.py
@@ -11,5 +11,35 @@
         #print every step along the way
     #else
         #print ("Illegal Choice")
-   
+
+choice_str = input("(h)armonic, (m)ultiplication or (q)uit:")
+#series_int = int(input("Series length:"))
+egyptian_sum = 0
+while choice_str == "m" or choice_str == "h" or choice_str == "q":
+    if choice_str == "q":
+        break
+    #elif choice_str == "h":
     
+    elif choice_str == "m":
+        first_int = int(input("First integer: "))
+        second_int = int(input("Second integer: "))
+        if second_int % 2 == 0:
+            None
+        else: 
+            egyptian_sum += first_int
+        print(first_int, second_int)
+        while second_int > 1:
+            first_int = (first_int *2)
+            second_int = (second_int//2)
+            print(first_int,second_int)
+            if second_int % 2 == 0:
+                continue
+            else:
+                egyptian_sum += first_int
+        print("Product:",egyptian_sum)
+    egyptian_sum = 0
+    choice_str = input("(h)armonic, (m)ultiplication or (q)uit:")
+
+
+
+
