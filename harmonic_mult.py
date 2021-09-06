83d8927eaf0ae46471db661f5a16646947db90c6
Gudmundur Kristjan <gummigudna@gmail.com>
2021-09-06 13:36:09 +0000

    reverted previous changes

--- harmonic_mult.py
+++ harmonic_mult.py
@@ -3,13 +3,13 @@
         #Quit
     #elif (h)
         #print first n numbers in Harmonc Series
-        #One result per line #sum at bottom (edit)
+        #One result per line
+        #sum at bottom (edit)
         #All numbers rounded with 4 extra digits
     #elif (m)
         #print ancient egyption multiplication
         #print every step along the way
     #else
         #print ("Illegal Choice")
-
-#text added   
+   
     
